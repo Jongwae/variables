86ae17f0b05d9ac23044e8eebc841dcfa182a92f
Jongwae <78085393+Jongwae@users.noreply.github.com>
2021-02-24 14:48:46 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#An onteger assignment
+age=45
+#A floating point
+salary=1456.8
+#A string
+name="John"
+print(age)
+print(salary)
+print(name)
+#!/usr/bin/python
+a=b=c=10
+print(a)
+print(b)
+print(c)
+
+a,b,c1=1,20.2,"python programming"
+print(a)
+print(b)
+print(c)
